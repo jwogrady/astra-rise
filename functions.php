69bd3a2f9d09241f0a0b840e6eb2366f9ef7c297
jwogrady <john@status26.com>
2025-10-19 08:37:33 -0500

    generate astra child theme

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * astra-rise Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package astra-rise
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_ASTRA_RISE_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'astra-rise-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_RISE_VERSION, 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
